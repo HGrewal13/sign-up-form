6c14f9ed74bce03fc6e7def67f1c7dda11d06ade
HGrewal13 <harrysgrewal13@gmail.com>
2025-05-09 22:18:24 -0400

    validity check for email, phone and password complete. confirmPassword still needs validation

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
-console.log("hi");
 
-// Define Input Variables
+// Define Form and Input Variables------------------------------------------------------------------------------------------------------------------------------------------------
 const form = document.querySelector("#signUpForm");
 const fName = document.querySelector("#fName");
 const lName = document.querySelector("#lName");
@@ -8,9 +7,8 @@ const email = document.querySelector("#email");
 const phone = document.querySelector("#phone");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirmPassword");
-const specialCharacters = ["@","#","!","$"];
 
-const fNameMsg = document.querySelector("#fNameMsg");
+// Functions------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 function checkFNameValidity() {
     if(fName.validity.valueMissing) {
@@ -66,22 +64,22 @@ function checkEmailValidity() {
     if(email.validity.valueMissing) {
         const msg = "Please enter your email";
         email.setCustomValidity(msg);
-        email.reportValidity(msg);
+        email.reportValidity();
         return false;
     } else if(email.validity.typeMismatch) {
         const msg = "Please enter in a valid email";
         email.setCustomValidity(msg);
-        email.reportValidity(msg);
+        email.reportValidity();
         return false;
     } else if(!checkEmailProvider(email.value)) {
         const msg = "email must be from: gmail, hotmail, yahoo or outlook";
         email.setCustomValidity(msg);
-        email.reportValidity(msg);
+        email.reportValidity();
         return false;
     } else if(!tldCheck.test(email.value)) {
         const msg = "Must end with .com";
         email.setCustomValidity(msg);
-        email.reportValidity(msg);
+        email.reportValidity();
         return false;
     } else {
         email.setCustomValidity("");
@@ -90,9 +88,95 @@ function checkEmailValidity() {
     }
 }
 
+// This function takes care of making sure the phone number is only 10 characters long. (12 including dashes)
+function phoneFormatting() {
+    let tel = phone.value;
+    // Replace all instances of "-" to avoid double dashing
+    tel = tel.replaceAll("-", "");
+    // length of tel is now considered without the "-". This allows the conditions to insert a "-" at the appropriate indexes
+    if(tel.length > 3 && tel.length <=6) {
+        tel = tel.slice(0,3) + "-" + tel.slice(3);
+        console.log(tel);
+    } else if(tel.length > 6) {
+        tel = tel.slice(0,3) + "-" + tel.slice(3,6) + "-" + tel.slice(6,10);
+    }
+    phone.value = tel;
+}
+
+// checks to see if the the key the user is pressing is a number or backspace. If not, don't allow the input
+function checkPhoneEntry(event) {
+    let regexNumberCheck = /^[0-9]+$/;
+    if(!regexNumberCheck.test(event.key) && event.key !== "Backspace") {
+        event.preventDefault();
+    }
+}
+
+function checkPhoneValidity() {
+    // phone.checkValidity();
+    if(phone.validity.valueMissing) {
+        let msg = "Phone number is required.";
+        phone.setCustomValidity(msg);
+        phone.reportValidity();
+        return false;
+    } else if(phone.value.length < 12) {
+        let msg = "Must be a valid phone number of 10 digits.";
+        phone.setCustomValidity(msg);
+        phone.reportValidity();
+        return false;
+    } else {
+        phone.setCustomValidity("");
+        phone.reportValidity();
+        return true;
+    }
+}
+
+function checkSymbolInclusion(password) {
+    let val = password.value;
+    let specialCharacters = ["@","#","!","$"];
+    let symbolFlag = false;
+    specialCharacters.forEach((symbol) => {
+        if(val.includes(symbol)) {
+            symbolFlag = true;
+        }
+    })
+    return symbolFlag;
+}
+
+function checkPasswordValidity() {
+    if(password.validity.valueMissing) {
+        let msg = "Password is required.";
+        password.setCustomValidity(msg);
+        password.reportValidity();
+        return false;
+    } else if(password.value.length < 4) {
+        let msg = "Password must be at least 4 characters long."
+        password.setCustomValidity(msg);
+        password.reportValidity();
+        return false;
+    } else if(!checkSymbolInclusion(password)) {
+        let msg = 'Password must contain a special character ("@","#","!","$").'
+        password.setCustomValidity(msg);
+        password.reportValidity();
+        return false;
+    }
+    else {
+        password.setCustomValidity("");
+        password.reportValidity();
+        return true;
+    }
+}
+
+
+
+// EVENT LISTENERS---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Need to listen for input changes to ensure valid pseudoclass is active correctly
 // fName.addEventListener("input", checkFNameValidity);
+
+// keydown event runs the function to allow only numbers or backspace to be inputted
+// After that we will use an input event listener and format the phone number with dashes
+phone.addEventListener("keydown", (event) => {checkPhoneEntry(event)});
+phone.addEventListener("input", phoneFormatting);
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -101,6 +185,8 @@ form.addEventListener("submit", (event) => {
     if(!checkFNameValidity()) return;
     if(!checkLNameValidity()) return;
     if(!checkEmailValidity()) return;
+    if(!checkPhoneValidity()) return;
+    if(!checkPasswordValidity()) return;
 })
 
 
